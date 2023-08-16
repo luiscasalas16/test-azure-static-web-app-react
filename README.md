e481448fd276bcb3a0ede3307e2247c74a20f31c
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2023-08-16 17:18:12 -0600

    ejemplo

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # test-azure-static-web-app-react
+
+Los comandos para generarla son:
+
+- npm create vite@latest
+  - √ Project name: example-react
+  - √ Select a framework: » React
+  - √ Select a variant: » TypeScript
+
+Los comandos para ejecutarla son:
+
+- npm install
+- npm run dev
+- npm run build
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="content" role="main">
+      <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg" viewBox="-11.5 -10.23174 23 20.46348">
+        <title>React Logo</title>
+        <circle cx="0" cy="0" r="2.05" fill="#61dafb" />
+        <g stroke="#61dafb" stroke-width="1" fill="none">
+          <ellipse rx="11" ry="4.2" />
+          <ellipse rx="11" ry="4.2" transform="rotate(60)" />
+          <ellipse rx="11" ry="4.2" transform="rotate(120)" />
+        </g>
+      </svg>
+      <h1>test-azure-static-web-app-react</h1>
+      <p>Test Application</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
